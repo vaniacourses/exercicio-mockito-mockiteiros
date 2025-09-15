91a88bff9419bc56d894424d7b2e66916ce771f2
Ramon-Rabello <ramonrabellorj@gmail.com>
2025-09-15 19:40:14 -0300

    TestesMockito

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,57 @@
-package jogo;
+import jogo.Jogo;
+import jogo.Dado;
+import jogo.Jogador;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class JogoTest {
-	
+class JogoTest {
 
+    @Test
+    void deveGanharCom7Turno1() {
+        Jogador jogador = mock(Jogador.class);
+        Dado d1 = mock(Dado.class);
+        Dado d2 = mock(Dado.class);
+
+        when(jogador.lancar(d1, d2)).thenReturn(7);
+
+        Jogo jogo = new Jogo(jogador, d1, d2);
+        assertTrue(jogo.jogar());
+    }
+
+    @Test
+    void devePerderCom2NoTurno1() {
+        Jogador jogador = mock(Jogador.class);
+        Dado d1 = mock(Dado.class);
+        Dado d2 = mock(Dado.class);
+
+        when(jogador.lancar(d1, d2)).thenReturn(2);
+
+        Jogo jogo = new Jogo(jogador, d1, d2);
+        assertFalse(jogo.jogar());
+    }
+
+    @Test
+    void deveGanharNoTerceiroLançamento() {
+        Jogador jogador = mock(Jogador.class);
+        Dado d1 = mock(Dado.class);
+        Dado d2 = mock(Dado.class);
+
+        when(jogador.lancar(d1, d2)).thenReturn(8, 5, 8);
+
+        Jogo jogo = new Jogo(jogador, d1, d2);
+        assertTrue(jogo.jogar());
+    }
+
+    @Test
+    void devePerderNoSegundoTurnoTirando7() {
+        Jogador jogador = mock(Jogador.class);
+        Dado d1 = mock(Dado.class);
+        Dado d2 = mock(Dado.class);
+
+        when(jogador.lancar(d1, d2)).thenReturn(6, 7);
+
+        Jogo jogo = new Jogo(jogador, d1, d2);
+        assertFalse(jogo.jogar());
+    }
 }
